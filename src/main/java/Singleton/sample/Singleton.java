1364ae6066b63dea89cd193260063b6192a3a436
ningxy <16847955@qq.com>
2019-10-21 17:45:10 +0800

    Singleton模式

--- src/main/java/Singleton/sample/Singleton.java
+++ src/main/java/Singleton/sample/Singleton.java
@@ -1,10 +1,20 @@
 package Singleton.sample;
 
 /**
+ * 饿汉式单例模式
+ *
+ * 特点：
+ * <ol>
+ *     <li>线程安全，调用效率高。</li>
+ *     <li>单例对象直接创建完成，实现简单。</li>
+ *     <li>可能会造成不必要的资源浪费。</li>
+ * </ol>
+ *
  * @author ningxy
  * @date 2019/10/21
  */
 public class Singleton {
+//    单例对象直接创建完成。
     private static Singleton singleton = new Singleton();
 
     private Singleton() {
